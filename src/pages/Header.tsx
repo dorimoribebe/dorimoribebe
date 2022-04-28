323560e205de4c0e9091f711905d9efb2418c853
dorimoribebe <dorimori_bebe@naver.com>
2022-04-28 14:17:17 +0900

    flask 대시보드용 버튼 추가

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,11 +1,23 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const FlaskUrl: string = "";
   return (
-    <div className="header">
-      <Link to="/" className="text-link">
-        #MOTD✨
-      </Link>
-    </div>
+    <header>
+      <div className="title ">
+        <Link to="/" className="text-link">
+          #MOTD✨
+        </Link>
+      </div>
+
+      <div
+        className="flask"
+        onClick={() => {
+          window.open(FlaskUrl, "_blank");
+        }}
+      >
+        What's the TREND👀
+      </div>
+    </header>
   );
 }
